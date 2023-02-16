3708525d9bfa96dd0681e541fe4a00ed21579f16
jay13029 <jay13029@gmail.com>
2023-02-17 00:47:27 +0800

    Set static files and pages

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+// require packages used in the project
 const express = require('express')
 const app = express()
 const port = 3000
-
 const exphbs = require('express-handlebars')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
 
+// routes setting
 app.get('/', (req, res) => {
     // res.send('This is my restaurant list')
     res.render('index')
 
 })
 
+// start and listen on the Express server
 app.listen(port, () => {
     console.log(`App is listening on localhost:${port}`)
 })
