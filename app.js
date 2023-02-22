28a461ebe90947afa62cf77d762188b7422aa2f8
jay13029 <jay13029@gmail.com>
2023-02-22 23:21:35 +0800

    Connect mongo db

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 // require packages used in the project
 const express = require('express')
+const exphbs = require('express-handlebars')
+const rstData = require('./restaurant.json')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
-const exphbs = require('express-handlebars')
 
-const rstData = require('./restaurant.json')
+// 資料庫連線設定
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error.')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
