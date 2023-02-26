f6204ff20a17d1805ad417ff2a780a22638589c3
jay13029 <jay13029@gmail.com>
2023-02-27 01:04:47 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
 // require packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const Restaurant = require('./models/restaurant')
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-
-// 資料庫連線設定
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error.')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-// setting static files
-app.use(express.static('public'))
-
 
 // start and listen on the Express server
 app.listen(port, () => {
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,36 +1,12 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
+
 const Restaurant = require('../restaurant')
-const rstData = require('../../restaurant.json')
-const rstAll = rstData.results
-
-// 資料庫連線設定
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error.')
-})
+const rstData = require('../../restaurant.json').results
 
 db.once('open', () => {
-  console.log('mongodb connected.')
-  for (const i in rstAll) {
-    const rstRow = rstAll[i]
-    Restaurant.create({
-      name: rstRow.name,
-      name_en: rstRow.name_en,
-      category: rstRow.category,
-      image: rstRow.image,
-      location: rstRow.location,
-      phone: rstRow.phone,
-      google_map: rstRow.google_map,
-      rating: rstRow.rating,
-      description: rstRow.description
+  Restaurant.create(rstData)
+    .then(() => {
+      console.log('seeder done!')
     })
-  }
-  console.log('done.')
+    .catch(err => console.log(err))
 })
